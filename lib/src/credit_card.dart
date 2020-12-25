0bbc184ff9b873f8f973efd397e5975e29fd9254
Philippe Fanaro <philippefanaro@gmail.com>
2020-12-25 11:30:30 -0300

    simplified the design of the credit card object (but deleted all the tests...)

--- lib/src/credit_card.dart
+++ lib/src/credit_card.dart
@@ -1,24 +1,89 @@
 import "package:meta/meta.dart";
 
+import "datetime_operator_extension.dart";
+
 @immutable
 class CreditCard {
+  final CardNumber number;
+  final Cvc cvc;
+  final CardHolderName name;
+  final DateTime date;
+
+  const CreditCard({
+    @required this.number,
+    @required this.cvc,
+    @required this.name,
+    @required this.date,
+  });
+}
+
+@immutable
+class CardNumber {
   static const int _minNumber = 1000000000000000;
 
   final int number;
-  final int code;
-  final String cardHolderName;
+
+  const CardNumber(this.number)
+      : assert(number ~/ _minNumber >= 1 && number ~/ _minNumber < 10);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CardNumber &&
+          runtimeType == other.runtimeType &&
+          number == other.number;
+
+  @override
+  int get hashCode => number;
+}
+
+@immutable
+class Cvc {
+  final int number;
+
+  const Cvc(this.number);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Cvc &&
+          runtimeType == other.runtimeType &&
+          number == other.number;
+
+  @override
+  int get hashCode => number;
+}
+
+@immutable
+class CardHolderName {
+  final String name;
+
+  CardHolderName(this.name) : assert(name.contains(" ") && name.isNotEmpty);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CardHolderName &&
+          runtimeType == other.runtimeType &&
+          name == other.name;
+
+  @override
+  int get hashCode => name.hashCode;
+}
+
+@immutable
+class ExpirationDate {
   final DateTime date;
 
-  CreditCard(this.number, this.code, this.cardHolderName, this.date)
-      : assert(number ~/ _minNumber >= 1 && number ~/ _minNumber < 10),
-        assert(cardHolderName.contains(" ") && cardHolderName.isNotEmpty),
-        assert(DateTime.now().year < date.year &&
-            (DateTime.now().year == date.year
-                ? DateTime.now().month < date.month
-                : true));
+  ExpirationDate(this.date) : assert(date < DateTime.now());
 
-  CreditCard.fromFormattedString(
-      String formattedNumber, int code, String cardHolderName, DateTime date)
-      : this(int.parse(formattedNumber.replaceAll(" ", "")), code,
-            cardHolderName, date);
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ExpirationDate &&
+          runtimeType == other.runtimeType &&
+          date.equals(other.date);
+
+  @override
+  int get hashCode => date.hashCode;
 }
--- lib/stripe_demo.dart
+++ lib/stripe_demo.dart
@@ -2,5 +2,6 @@
 /// {@category Payment, Stripe, API}
 library stripe_demo;
 
-export "src/credit_card.dart" show CreditCard;
+export "src/credit_card.dart"
+    show CreditCard, CardNumber, Cvc, CardHolderName, ExpirationDate;
 export "src/datetime_operator_extension.dart" show DateTimeOperatorExtension;
